9bf49512e77d4112d4f8dbde05e5e01286bf9d7f
Varga Dávid <nixtrox.vidi@gmail.com>
2021-04-17 21:57:18 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+
+
+function changeText(){
+    document.getElementById('0').innerHTML = '0';
+    document.getElementById('0_2').innerHTML = '0'; 
+    document.getElementById('1').innerHTML = '1';
+    document.getElementById('5').innerHTML = '5';
+    document.getElementById('5_2').innerHTML = '5'; 
+    document.getElementById('9').innerHTML = '9';
+    document.getElementById('eredmeny').innerHTML = 'Eredmény: 100';
+}
+
+function deleteText(){
+    document.getElementById('0').innerHTML = '';
+    document.getElementById('0_2').innerHTML = ''; 
+    document.getElementById('1').innerHTML = '';
+    document.getElementById('5').innerHTML = '';
+    document.getElementById('5_2').innerHTML = ''; 
+    document.getElementById('9').innerHTML = '';
+    document.getElementById('eredmeny').innerHTML = 'Eredmény: ';
+}
+
